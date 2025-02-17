c41946e39adf59213abaf5857690c943b5cebf40
Nycolas <nybertocco@gmail.com>
2025-02-17 12:40:18 -0300

    Alterção de link no menu

--- src/menu.jsx
+++ src/menu.jsx
@@ -6,7 +6,7 @@ export default function Menu() {
       <h1 className="h1-menu">Bem vindo à nossa loja!</h1>
       <a
         className="link-menu"
-        href="https://www.nike.com.br/?awc=17652_1739547154_82327d4b63dbde738bb30d93dafc6eb6&utm_source=Zanox&utm_medium=141629&utm_campaign=Opera+Software+AS&utm_content=gx1-br-awin-nike-sd-def&utm_term=Direct+Traffic&af_channel=141629"
+        href="https://www.instagram.com/loja.teenshop/"
         target="blank"
       >
         Visite nosso site!
